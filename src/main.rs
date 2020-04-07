9dce12d0b1f765091f2ea4a808328f548f5aab96
Nazarii Bardiuk <nazarii@bardiuk.com>
2020-04-07 19:30:49 +0100

    feat: error handling

--- src/main.rs
+++ src/main.rs
@@ -6,37 +6,14 @@ use std::process;
 
 fn main() -> io::Result<()> {
     let args: Vec<_> = env::args().skip(1).collect();
+    let mut lox = Lox { has_error: false };
     match args.len() {
-        0 => run_prompt(),
-        1 => run_file(&args[0]),
+        0 => lox.run_prompt(),
+        1 => lox.run_file(&args[0]),
         _ => {
             println!("Usage: rlox [script]");
             process::exit(64)
         }
-    }
-}
-
-fn run_file(path: &str) -> io::Result<()> {
-    run(&fs::read_to_string(path)?);
-    io::Result::Ok(())
-}
-
-fn run_prompt() -> io::Result<()> {
-    loop {
-        print!("> ");
-        io::stdout().flush()?;
-
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
-
-        run(&input)
-    }
-}
-
-fn run(source: &str) {
-    let tokens = scan_tokens(source);
-    for token in tokens {
-        println!("{:?}", token);
     }
 }
 
@@ -46,3 +23,44 @@ fn scan_tokens(source: &str) -> Vec<Token> {
 
 #[derive(Debug)]
 struct Token {}
+
+struct Lox {
+    has_error: bool,
+}
+
+impl Lox {
+    fn run(&self, source: &str) {
+        let tokens = scan_tokens(source);
+        for token in tokens {
+            println!("{:?}", token);
+        }
+    }
+    fn run_file(&self, path: &str) -> io::Result<()> {
+        self.run(&fs::read_to_string(path)?);
+        if self.has_error {
+            process::exit(65)
+        };
+        io::Result::Ok(())
+    }
+
+    fn run_prompt(&mut self) -> io::Result<()> {
+        loop {
+            print!("> ");
+            io::stdout().flush()?;
+
+            let mut input = String::new();
+            io::stdin().read_line(&mut input)?;
+
+            self.run(&input);
+            self.has_error = false;
+        }
+    }
+
+    fn error(&mut self, line: usize, message: &str) {
+        self.report(line, "", message);
+    }
+    fn report(&mut self, line: usize, location: &str, message: &str) {
+        println!("[line {}] Error{}: {}", line, location, message);
+        self.has_error = true;
+    }
+}
